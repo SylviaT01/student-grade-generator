a6aad8a5b8446ce660854c3cd371116d8b6b969f
Sylvia <sylviachebet03@gmail.com>
2024-03-21 11:22:57 +0300

    updated

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const h2 = document.createElement("h2");
-h2.textContent = "Student Grade";
-document.querySelector("body").appendChild(h2);
+function calculateGrade(){
+    let ift = document.querySelector("#ift").value;
+    let dst = document.querySelector("#dst").value;
+    let math = document.querySelector("#math").value;
+    let cls = document.querySelector("#cls").value;
+    let dsi = document.querySelector("#dsi").value;
+
+    let ttl = parseFloat(ift) + parseFloat(dst) + parseFloat(math) + parseFloat(cls) + parseFloat(dsi);
+    let prt = (ttl * 100)/500;
+    if (prt >79){
+        document.querySelector(".grd").innerHTML = "A";
+    } else if (prt > 60 && prt <=79){
+        document.querySelector(".grd").innerHTML = "B";
+    } else if (prt >49 && prt <= 59){
+        document.querySelector(".grd").innerHTML = "C";
+    } else if (prt > 40 && prt <= 49){
+        document.querySelector(".grd").innerHTML = "D";
+    } else {
+        document.querySelector(".grd").innerHTML = "E";
+    }
+    document.querySelector(".ttl").innerHTML = ttl;
+    document.querySelector(".prt").innerHTML = prt;
+}
